8c5c05354a2bbaee07a63c23e72f7b8cfa85cada
ksurendra2004 <k.surendra2004@gmail.com>
2023-01-04 12:06:09 +0530

    Removed unused code.

--- src/main/java/com/itemcatalog/ItemCatalogApplication.java
+++ src/main/java/com/itemcatalog/ItemCatalogApplication.java
@@ -22,13 +22,4 @@ public class ItemCatalogApplication {
 				.apis(RequestHandlerSelectors.basePackage("com.itemcatalog")).build();
 	}
 
-	/*@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build()
-				.pathMapping("/");
-	}*/
 }
